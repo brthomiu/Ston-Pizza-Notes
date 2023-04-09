a0f265a6f8fb8c4e83a218143fb2fa47752139b1
Brad <brthomiu@gmail.com>
2023-04-08 21:45:56 -0600

    Pre-deploy test

--- package.js
+++
@@ -1,8 +0,0 @@
-// just stashing this here
-// {
-//       "scripts": {
-//     "start": "node server/dist/index.js",
-//     "build": "tsc",
-//     "heroku-postbuild": "NPM_CONFIG_PRODUCTION=false npm install --prefix client && npm run build --prefix client"
-//   }
-// }
