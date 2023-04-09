21b472d0628517e5990a49826bbc202f2cb2de70
Brad <brthomiu@gmail.com>
2023-04-09 14:38:52 -0600

    Remove another cursed reference to pizza...

--- client/src/components/ConfirmDelete.jsx
+++ client/src/components/ConfirmDelete.jsx
@@ -11,8 +11,8 @@ const ConfirmDelete = (props) => {
     <div className="confirmDelete--bg">
       <div className="confirmDelete--container">
         <div className="confirmDelete--content">
-          <h2>Are you sure you want to delete this pizza?</h2>
-          <h3>Deleted pizzas cannot be retrieved.</h3>{" "}
+          <h2>Are you sure you want to delete this recipe?</h2>
+          <h3>Deleted recipes cannot be retrieved.</h3>{" "}
           <span>
             <button onClick={() => props.setShowConfirmation(false)}>
               Back
